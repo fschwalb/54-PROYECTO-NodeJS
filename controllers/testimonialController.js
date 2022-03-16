6dfe9210f551244ff8604ca90a3aa2daeb73c9c3
Fernando Schwalb <fschwalb@gmail.com>
2022-03-16 20:59:10 -0300

    Testimoniales en DB Added

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,5 +1,6 @@
+import { Testimonial } from '../models/Testimoniales.js';
 
-const guardarTestimonial = (req, res) => {
+const guardarTestimonial = async (req, res) => {
 
     // Validar
     const { nombre, correo, mensaje } = req.body;
@@ -8,15 +9,39 @@ const guardarTestimonial = (req, res) => {
 
     if ( nombre.trim() === '' ) {
         errores.push({ mensaje: 'El nombre está vacío' });
-    }
+    };
 
     if ( correo.trim() === '' ) {
         errores.push({ mensaje: 'El correo está vacío' });
-    }
+    };
 
     if ( mensaje.trim() === '' ) {
         errores.push({ mensaje: 'El mensaje está vacío' });
-    }
+    };
+
+    if ( errores.length > 0 ) {
+        // Mostrar la vista con Errores
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje
+        });
+    } else {
+        // Almacenar en la DB
+        try {
+            await Testimonial.create({
+                nombre,
+                correo,
+                mensaje
+            });
+
+            res.redirect('testimoniales');
+        } catch (error) {
+            console.log(error);
+        };
+    };
 };
 
 export {
